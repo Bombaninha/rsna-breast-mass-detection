42501618616452088238a409b718b436492be186
MatheusCGodoy <matheusgodoy2000@gmail.com>
2023-04-04 02:00:14 -0300

    Saves the results from the tests of the last epoch.

--- train_test.py
+++ train_test.py
@@ -7,13 +7,19 @@ import torchvision.transforms as transforms
 from sklearn.model_selection import StratifiedShuffleSplit
 import splitfolders
 
-torch.backends.cudnn.benchmark = True
-torch.set_default_tensor_type('torch.cuda.HalfTensor')
+in_device = input('Choose a device to run on (cuda or cpu): ')
 
-# Set device to GPU if available
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if(in_device == 'cuda'):
+    # Set device to GPU if available
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    torch.backends.cudnn.benchmark = True
+    torch.set_default_tensor_type('torch.cuda.HalfTensor')
+else:
+    device = torch.device('cpu')
 
 splitfolders.ratio('results', output='output', seed=1337, ratio=(0.8, 0, 0.2)) 
+
+class_names = ['1', '2', '3', '4a', '4b', '4c', '5', '6']
 
 # Define the transformation to apply to the images
 def get_transform(image_size, num_channels):
@@ -38,8 +44,8 @@ def get_dataset(image_size, num_channels):
 # Define the data loader
 def get_data_loader(image_size, num_channels, batch_size):
     train_dataset, test_dataset = get_dataset(image_size, num_channels)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, pin_memory=True, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, pin_memory=True, shuffle=False)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, generator=torch.Generator(device=device), shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, generator=torch.Generator(device=device), shuffle=False)
     return train_loader, test_loader, train_dataset, test_dataset
 
 # Define the model architecture
@@ -71,12 +77,12 @@ class CNN(nn.Module):
         x = self.fc2(x)
         return x
 
-# Define the loss function and optimizer
-def get_model(image_size, num_channels):
-    model = CNN(image_size, num_channels).to(device)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters())
-    return model, criterion, optimizer
+# # Define the loss function and optimizer
+# def get_model(image_size, num_channels):
+#     model = CNN(image_size, num_channels).to(device)
+#     criterion = nn.CrossEntropyLoss()
+#     optimizer = optim.Adam(model.parameters())
+#     return model, criterion, optimizer
 
 
 # Initialize the model and move it to the GPU if available
@@ -86,7 +92,7 @@ model = CNN((544, 814), 1).to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-train_loader, test_loader, train_dataset, test_dataset = get_data_loader((544, 814), 1, 4)
+train_loader, test_loader, train_dataset, test_dataset = get_data_loader((544, 814), 1, 8)
 
 # Train the model
 num_epochs = 10
@@ -108,19 +114,25 @@ for epoch in range(num_epochs):
     train_loss = train_loss / len(train_dataset)
     train_accuracy = train_correct / len(train_dataset)
     
+    predicted_lst = []
+    labels_lst = []
+
     # Evaluate the model on the test set
     test_loss = 0.0
     test_correct = 0
     model.eval()
     with torch.no_grad():
         for images, labels in test_loader:
+            labels_lst.append(labels)
             images = images.to(device)
             labels = labels.to(device)
             outputs = model(images)
+            # print('output: ' + str(outputs))
             loss = criterion(outputs, labels)
             test_loss += loss.item() * images.size(0)
             _, predicted = torch.max(outputs.data, 1)
             test_correct += (predicted == labels).sum().item()
+            predicted_lst.append(predicted)
     test_loss = test_loss / len(test_dataset)
     test_accuracy = test_correct / len(test_dataset)
     
@@ -130,3 +142,12 @@ for epoch in range(num_epochs):
     
     torch.cuda.empty_cache()
 
+    if epoch == num_epochs-1: # record results from the last epoch tests
+        fp = open('data/cnn_predicted' + str(epoch+1) + '.txt', 'w') 
+        for i in range(len(predicted_lst)):
+            pred_labels, class_labels = zip(*[(class_names[pl], class_names[cl]) for pl, cl in zip(predicted_lst[i].tolist(), labels_lst[i].tolist())])
+            #class_labels = [class_names[j] for j in labels_lst[i].tolist()]
+            
+            for k in range(len(class_labels)):
+                fp.write('Epoch [{}/{}] - Batch {}: predicted {} was class {}\n'.format( 
+                        epoch+1, num_epochs, i, pred_labels[k], class_labels[k]))
